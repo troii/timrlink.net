33aedcb72409e73188a3126d3dd9c95532788a06
Bernhard Danecker <bernhard.danecker@troii.com>
2023-05-24 12:22:42 +0200

    adding ConfigurationException

--- timrlink.net.Core/MissingConfigurationException.cs
+++ timrlink.net.Core/MissingConfigurationException.cs
@@ -1,8 +1,28 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace timrlink.net.Core
 {
-    public class MissingConfigurationException : Exception
+    public class ConfigurationException : Exception
+    {
+        public ConfigurationException()
+        {
+        }
+
+        protected ConfigurationException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+
+        public ConfigurationException(string message) : base(message)
+        {
+        }
+
+        public ConfigurationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+    
+    public class MissingConfigurationException : ConfigurationException
     {
         public MissingConfigurationException(string section, string parameter)
             : base($"Missing parameter '{parameter}' in section '{section}'.")
